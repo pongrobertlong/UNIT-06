486f8e6c90584065bec217a26a8325b7f97d0c47
Robert Long <pongrobertlong@gmail.com>
2020-06-06 16:37:04 -0400

    Updated JavaScript
    - Remove Hearts
    - Added CheckWin Function

--- js/app.js
+++ js/app.js
@@ -1,9 +1,15 @@
 // Variables
 const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
-const missed = 0;
+let missed = 0;
 const startGame = document.querySelector('.btn__reset');
 const overlay = document.getElementById('overlay');
+const ul = document.querySelector('#phrase ul');
+const createBtn = document.createElement('a');
+const tries = document.querySelectorAll('.tries');
+const scoreBoard = document.querySelector('#scoreboard ol');
+
+
 
 
 
@@ -16,11 +22,11 @@ startGame.addEventListener('click', function() {
 //Phrase Array
 
 const phrases = [
-    "Power Rangers",
-    "Catdog",
-    "Rugrats",
-    "Toy Story",
-    "Chicken Little"
+    "power rangers",
+    "catdog",
+    "rugrats",
+    "toy story",
+    "chicken little"
 ];
 
 
@@ -38,7 +44,6 @@ function getRandomPhraseAsArray(arr){
 function addPhrasetoDisplay(arr) {
     for (let i = 0; i < arr.length; i ++) {
         const li = document.createElement('li'); // Creating a new li
-        const ul = document.querySelector('#phrase ul'); // Selecting the id phrase and ul inside
         const text = document.createTextNode(arr.charAt([i])); // looping through each character inside the string
         li.appendChild(text); 
         ul.appendChild(li);
@@ -64,17 +69,16 @@ addPhrasetoDisplay(phraseArray);
 
 function checkLetter (btnPressed) {
     const li = document.querySelectorAll('.letter');
-    console.log(li);
     let match = null;
     for (let i = 0; i < li.length; i++) {
-        if (btnPressed.toUpperCase() == li[i].textContent.toUpperCase) {
-            match = true;
-            li.className = "show letter";
+        if (btnPressed == li[i].textContent) {
+            match = li[i].textContent;
+            li[i].className = "show letter";
         }
         
-    
+       
     }
-    
+    return match;
 }
 
 // Event Listener for Keyboard
@@ -86,9 +90,89 @@ qwerty.addEventListener('click', (e) => {
         btnPressed.className = "chosen";
         btnPressed.setAttribute('disabled', true);
         const letterFound = checkLetter(btnPressed.textContent)
-        console.log(letterFound)
+        if (letterFound == null) {
+            const tries = document.querySelectorAll('.tries')[0];
+            tries.parentNode.removeChild(tries);
+            missed += 1;
+        }
     }
-    
+    checkWin()
 });
+
+// Check Win Function
+
+function checkWin (){
+    const show = document.querySelectorAll('.show');
+    const letters = document.querySelectorAll('.letter');
+    const headlineText = document.querySelector('.title');
+    if(show.length === letters.length) {
+        overlay.className = "win"
+        overlay.style.display = "flex";
+        headlineText.textContent = "Congratulations! You Won!"
+        // overlay.appendChild(createBtn);
+        // createBtn.className = "btn__reset";
+        // createBtn.textContent = "Reset";
+        resetGame();
+    }
+    if(missed > 4) {
+        overlay.className = "lose";
+        overlay.style.display = "flex";
+        headlineText.textContent = "Sorry! You Lose! Try Again!"
+        // overlay.appendChild(createBtn);
+        // createBtn.className = "btn__reset";
+        // createBtn.textContent = "Reset";
+        // resetGame();
+        }
+};
+
+// //Reset Game 
+
+// function resetGame () {
+ 
+//     createBtn.addEventListener('click', ()=>{  
+     
+// //         const qwertyBtn = document.querySelectorAll('.chosen');
+// //         const li = document.createElement('li');
+// //         const img = document.createElement('img');
+// //          overlay.style.display = "none";
+
+// //     // Generate new Phrase
+// //         const phraseLetters = phrase.children.length;
+// //         for (let i = 0; i < phraseLetters; i ++) {
+// //             const li = phrase.children[0];
+// //             phrase.removeChild(li);
+// //         }
+
+// //         const phraseArray = getRandomPhraseAsArray(phrases);
+// //         addPhrasetoDisplay(phraseArray); 
+
+
+
+// //      // Loop for the qwerty buttons
+// //         for(let i = 0; i < qwertyBtn.length; i ++) {
+// //             qwertyBtn[i].className = "";
+// //             qwertyBtn[i].setAttribute('disabled', false); 
+// //         }
+// //     //Loop for Hearts 
+// //         for(let i = 0; i < tries.length; i++) {
+// //             if (tries[i] !== 5) {
+// //                 scoreBoard.append(li);
+// //                 li.className = "tries";
+// //                 li.appendChild(img);
+// //                 img.src = "images/liveHeart.png";
+// //                 img.style.height = "35px";
+// //                 img.style.width = "30px";
+                
+                
+                
+// //             }
+            
+// //          }   
+   
+
+//     });
+    
+
+// };
 
 
